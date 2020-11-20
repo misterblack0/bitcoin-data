2dd08f7c73bb650d503a9437b9fef4709e52fe79
misterblack0 <misterblack0@protonmail.com>
2020-11-20 16:47:48 +0200

    routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from "../../styles/Header.module.scss";
 
 export default function Header() {
@@ -5,10 +6,23 @@ export default function Header() {
     <div className={styles.container}>
     <div className={styles.logo}><h1>Logo</h1></div>
       <div className={styles.navbar}>
-          
-          <a href="#" className={styles.link}>Overview</a>
-          <a href="#" className={styles.link}>Blocks</a>
-          <a href="#" className={styles.link}>Graphs</a>
+      <ul>
+      <li>
+        <Link href="/">
+          <a>Overview</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/blocks">
+          <a>Blocks</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/graphs">
+          <a>Graphs</a>
+        </Link>
+      </li>
+    </ul>
       </div>
     </div>
   );
