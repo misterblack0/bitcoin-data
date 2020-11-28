8a1b49030654cbb3e168eeeb78d382c75ac6ee82
misterblack0 <misterblack0@protonmail.com>
2020-11-28 18:20:18 +0200

    progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -10,16 +10,12 @@ export default function ProgressBar(props) {
   return (
     <div className={styles.container}>
 
-<div className={styles.container2}>
-<div className={styles.container3}>
-<div className={styles.container4}>
+
 
       <div className={styles.filler} style={fillerStyles}>
         <span className={styles.label}>{`${completed}%`}</span>
       </div>
       </div>
-    </div>
-    </div>
-    </div>
+ 
   );
 }
