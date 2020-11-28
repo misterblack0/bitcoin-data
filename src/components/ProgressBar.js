5635f740fc42b8a53e531c2d066a11229232c9b9
misterblack0 <misterblack0@protonmail.com>
2020-11-28 18:00:02 +0200

    progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,26 +1,25 @@
 import styles from "../../styles/ProgressBar.module.scss";
 
-const ProgressBar = (props) => {
-  const { bgcolor, completed } = props;
+export default function ProgressBar(props) {
+  const { completed } = props;
 
   const fillerStyles = {
-    height: "100%",
     width: `${completed}%`,
-    backgroundColor: "#6a1b9a",
-    borderRadius: "inherit",
-    transition: "width 1s ease-in-out",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
   };
 
   return (
     <div className={styles.container}>
-      <div style={fillerStyles}>
+
+<div className={styles.container2}>
+<div className={styles.container3}>
+<div className={styles.container4}>
+
+      <div className={styles.filler} style={fillerStyles}>
         <span className={styles.label}>{`${completed}%`}</span>
       </div>
+      </div>
+    </div>
+    </div>
     </div>
   );
-};
-
-export default ProgressBar;
+}
