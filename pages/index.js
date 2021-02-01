dd49de96114cf0eb6c6b665c8e7fd1b73fdc3f9c
misterblack0 <misterblack0@protonmail.com>
2021-02-01 17:54:22 +0200

    head meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Layout from "../src/components/layout";
 import Fees from "../src/components/sections/Fees";
 import UnconfirmedTxs from "../src/components/sections/UnconfirmedTxs";
@@ -7,11 +8,17 @@ import LatestBlocks from "../src/components/sections/LatestBlocks";
 
 export default function IndexPage() {
     return (
-        <Layout>
-            <Fees />
-            <UnconfirmedTxs />
-            <LatestTxs />
-            <LatestBlocks />
-        </Layout>
+        <>
+            <Head>
+                <meta property="og:title" content="OnChain Data" key="title" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Layout>
+                <Fees />
+                <UnconfirmedTxs />
+                <LatestTxs />
+                <LatestBlocks />
+            </Layout>
+        </>
     );
 }
