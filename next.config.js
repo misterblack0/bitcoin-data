3971c424206bb94ed1459483fde9e9de052e270e
misterblack0 <47751056+misterblack0@users.noreply.github.com>
2021-02-11 13:26:37 +0200

    cors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 // matching all API routes
-                source: "/index",
+                source: "/index.js",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
