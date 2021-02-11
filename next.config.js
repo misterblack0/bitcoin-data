68ec1a4369818d98ac8d8b1cc4f84d286cfe3747
misterblack0 <47751056+misterblack0@users.noreply.github.com>
2021-02-11 10:39:48 +0200

    enabling cors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 // matching all API routes
-                source: "https://mempool.space/api/mempool*",
+                source: "https://mempool.space/api/mempool",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
