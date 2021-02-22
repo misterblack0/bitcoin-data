98abdb247a81bb8f1549d5a34242446342a79056
misterblack0 <47751056+misterblack0@users.noreply.github.com>
2021-02-22 19:18:37 +0200

    cors

--- src/components/fetcher.js
+++ src/components/fetcher.js
@@ -1,7 +1,7 @@
 export const fetcher = async (url) => {
     const res = await fetch(url);
-    // eslint-disable-next-line no-undef
     console.log(res);
+    // eslint-disable-next-line no-undef
     // If the status code is not in the range 200-299,
     // we still try to parse and throw it.
     if (!res.ok) {
